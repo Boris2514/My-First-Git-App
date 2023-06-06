0769c4503f64c9a945e5ad4447aa00787eb73a2e
Boris2514 <borismiashiro@gmail.com>
2023-06-05 20:52:26 -0400

    fourth commit

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 let $btn = document.querySelector(".hello");
 let $p = document.querySelector(".sayHello");
 
-console.log($btn, $p);
 function sayHello() {
   return "Hello my friends";
 }
@@ -9,3 +8,10 @@ function sayHello() {
 $btn.addEventListener("click", (e) => {
   $p.innerHTML = sayHello();
 });
+
+let array = [1, 2, 3, 4, 5, 6, 7];
+for (let i = 0; i < array.length; i++) {
+  let sum = 0;
+  sum += array[i];
+  console.log(sum);
+}
