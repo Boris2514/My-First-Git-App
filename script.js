35c6c9e37c331130789f051dd77b40e704fb0d90
Boris2514 <borismiashiro@gmail.com>
2023-06-05 20:20:41 -0400

    third commit

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+let $btn = document.querySelector(".hello");
+let $p = document.querySelector(".sayHello");
+
+console.log($btn, $p);
 function sayHello() {
-  console.log("hello");
+  return "Hello my friends";
 }
+
+$btn.addEventListener("click", (e) => {
+  $p.innerHTML = sayHello();
+});
